25df8138b473fe06b4ddf64feb5b7bf6732995d9
Fraser Mince <frasermince@Fraser-Minces-MacBook-Pro.local>
2022-08-26 16:38:54 -0500

    Fix logging.

--- src/muzero.py
+++ src/muzero.py
@@ -30,7 +30,7 @@ class GlobalParamsActor(object):
 
 def main(argv):
     ray.init(address='auto')
-    print("***RESOURCES", ray.global_state.cluster_resources())
+    print("***RESOURCES", ray.nodes())
     flags.mark_flag_as_required('config')
     print(sys.argv[1])
     rollout_size = 5
